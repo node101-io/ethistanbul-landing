422ad97ac244c0d2f5bef58742e0fe693b5cd91f
Necip Baha Sağıroğlu <sagiroglun22@itu.edu.tr>
2025-05-09 17:21:20 +0300

    resize footer image

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import FooterImage from "@/assets/footer_image.png";
+import FooterImage from "@/assets/footer_image.webp";
 
 const Footer = () => {
   return (
